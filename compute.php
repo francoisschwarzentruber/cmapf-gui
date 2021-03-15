d6839b35083941312fe34eda719fbffbf9c6db16
aqueffelec <arthur.queffelec@irisa.fr>
2021-03-15 14:30:06 +0100

    Creation of python script for computation; Untested php code

--- compute.php
+++ compute.php
@@ -1,8 +1,13 @@
- <?php
+<?php
 /*input
 $_POST["init"] //of the form [1, 2, 3]
 $_POST["target"] //idem
 $_POST["radius"] a number
 $_POST["pngFileName"] //name of the PNG file, example: myfunnymaze.png*/
-echo("[[1, 3, 5], [4, 3, 2]]");
+// echo("[[1, 3, 5], [4, 3, 2]]");
+
+$command = escapeshellcmd('compute.py "$_POST["init"]" "$_POST["target"]" $_POST["radius"] "$_POST["pngFileName"]"');
+$output = shell_exec($command);
+echo $output;
+
 ?>
