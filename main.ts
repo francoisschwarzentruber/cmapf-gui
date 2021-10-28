b60c0a8f6aa4c442c563323d6508554253815d56
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-10-28 11:04:48 +0200

    execution textarea

--- main.ts
+++ main.ts
@@ -14,7 +14,6 @@ window.onload = () => {
     const updateCurrentExecutionTextArea = () => {
         const textarea = (<HTMLTextAreaElement>document.getElementById("executions"));
         const nbLine = textarea.value.substr(0, textarea.selectionStart).split("\n").length-1;
-        console.log(nbLine)
         const line = textarea.value.split("\n")[nbLine];
         GUIExecution.loadFromString(line);
     };
