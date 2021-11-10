e16658dd7bb024d99ed0a2926447c5fc9f2a62ef
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-11-10 11:35:18 +0100

    text input

--- main.ts
+++ main.ts
@@ -20,5 +20,5 @@ window.onload = () => {
 
     textArea.onkeyup = updateCurrentExecutionTextArea;
     textArea.onclick = updateCurrentExecutionTextArea;
-    document.getElementById("helptextarea").onclick = () => alert("In the textbox, you can write either a configuration or an execution. Example of a configuration: <2, 3, 6> is a configuration of three agents where the first agent is at location 2, the second at 3 and the third at 6. Example of an execution: [[1, 4, 5], [2, 7, 8]] is an execution with 2 agents of length 3. The path of the first agent is 1, 4, 5. The path of the second agent is 2, 7, 8.")
+    document.getElementById("helptextarea").onclick = () => alert("In the textbox, each line can be either a configuration or an execution. Example of a configuration: <2, 3, 6> is a configuration of three agents where the first agent is at location 2, the second at 3 and the third at 6. Example of an execution: [[1, 4, 5], [2, 7, 8]] is an execution with 2 agents of length 3. The path of the first agent is 1, 4, 5. The path of the second agent is 2, 7, 8. The tool visualizes the configuration, or the execution of the current line.")
 }
